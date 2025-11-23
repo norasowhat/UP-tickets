9d128a08fb36fc060b8ae235889ef7d8537f11c6
arantxavalenciaa <valenciarantxa@gmail.com>
2025-11-23 03:09:12 -0600

    ADD professor with role and academy

--- src/app/com/agregar-profesor/agregar-profesor.ts
+++ src/app/com/agregar-profesor/agregar-profesor.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AcademyService } from '../../services/dp-academias';
 
 @Component({
   selector: 'app-agregar-profesor',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './agregar-profesor.html',
   styleUrl: './agregar-profesor.css',
 })
-export class AgregarProfesor {}
+
+export class AgregarProfesor implements OnInit {
+
+  academias: any[] = [];
+
+  profesor = {
+    ID:'',
+    nombre: '',
+    apellido_p: '',
+    apellido_m: '',
+    correo: '',
+    password: 'password1234',
+    telefono: '',
+    id_academia: '',
+    hiring_date:''
+  };
+
+  constructor(private academyService: AcademyService) {}
+
+  ngOnInit(): void {
+    this.getAcademias();
+  }
+
+  getAcademias() {
+    this.academyService.getAcademias().subscribe({        // <-- CORREGIDO
+      next: (resp) => {
+        this.academias = resp;
+        console.log("Academias cargadas:", this.academias);
+      },
+      error: (err) => {
+        console.error("Error cargando academias", err);
+      }
+    });
+  }
+
+
+  enviarFormulario() {
+    console.log("Datos enviados:", this.profesor);
+
+    this.academyService.agregarProfesor(this.profesor).subscribe({   // <-- CORREGIDO
+      next: (resp:any) => {
+        console.log("Respuesta del servidor:", resp);
+        alert("Profesor agregado correctamente");
+      },
+      error: (err:any) => {
+        console.error("Error en el servidor:", err);
+        alert("Error al guardar el profesor");
+      }
+    });
+  }
+}
---
+++ src/app/services/dp-academias.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AcademyService {
+  private apiUrl = 'http://localhost:8888/web/up-tickets-back/dp-academias.php';
+
+  constructor(private http: HttpClient) {}
+
+  getAcademias(): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'X-Auth-Token': token || ''
+    });
+
+    
+
+    return this.http.get<any>(this.apiUrl, { headers });
+  }
+
+  agregarProfesor(data: any): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'X-Auth-Token': token || ''
+    });
+
+    return this.http.post<any>(
+      'http://localhost:8888/web/up-tickets-back/add-profesor.php',
+      data,
+      { headers }
+    );
+  }
+}
+
+  
